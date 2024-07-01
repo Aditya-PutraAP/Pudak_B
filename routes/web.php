fc13d5d4958d09390b59cdf66e9c4aea1fd982d0
Aditya-PutraAP <adityaputra.ap543@gmail.com>
2024-07-01 19:44:33 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('frontend.home');
+});
+
+Route::get('/about', function () {return view('frontend.about');})->name('about');
